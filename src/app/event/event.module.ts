3b0504ade6f236c2bde4121aa1102b98009e6d2e
Alejandro <Alejandrehl@icloud.com>
2018-12-06 20:00:22 -0300

    Finish

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterModule } from './register/register.module';
+
 import {
   FormioResource,
   FormioResourceRoutes,
   FormioResourceConfig,
   FormioResourceService
 } from 'angular-formio/resource';
+import { ResourceComponent } from './resource/resource.component';
+import { ViewComponent } from './view/view.component';
 
+const eventRoutes: Routes = FormioResourceRoutes({
+  resource: ResourceComponent,
+  view: ViewComponent
+});
+eventRoutes[2].children.push({
+  path: 'registrations',
+  loadChildren: () => RegisterModule
+});
 @NgModule({
-  declarations: [],
+  declarations: [ResourceComponent, ViewComponent],
   imports: [
     CommonModule,
     FormioResource,
-    RouterModule.forChild(FormioResourceRoutes())
+    RouterModule.forChild(FormioResourceRoutes()),
+    RouterModule.forChild(eventRoutes)
   ],
   providers: [
     FormioResourceService,
